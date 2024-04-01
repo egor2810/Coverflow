2cd30759c3aa61dfb6f6d99c580020932ca57a1f
Ablogin Egor <47031969+egor2810@users.noreply.github.com>
2024-04-01 19:20:11 +0300

    Initial Commit

---
+++ Coverflow/CoverflowApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoverflowApp.swift
+//  Coverflow
+//
+//  Created by Егор Аблогин on 01.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct CoverflowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
